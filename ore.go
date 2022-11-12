5eb445aa8e9231225b28c6a0e405233d7108d6f7
memmaker <memmaker@32kb.org>
2022-11-12 20:55:51 +0100

    better zircon integration

--- ore.go
+++ ore.go
@@ -2,135 +2,55 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
-	"strconv"
 	"strings"
 )
 
 var endPoint = ValueOrDefault(os.Getenv("CRUDE_API_ENDPOINT"), "https://proxyman.local:8080")
 var apiKey = ValueOrDefault(os.Getenv("CRUDE_API_KEY"), "crude_api_key")
 
-func ensureDirExists(dir string) {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.Mkdir(dir, os.ModePerm)
-	}
-}
-
-func getFieldTypes() string {
-	response := queryRequest(apiKey, endPoint+"/models/fields")
-	return response.Body
-}
-
-func loadModelsFromAPI() ModelInfos {
-	apiResponse := queryRequest(apiKey, endPoint+"/models/browse")
-	var response ModelResponse
-	json.Unmarshal([]byte(apiResponse.Body), &response)
-	return ModelInfos{Models: response.Models, ModelNames: response.ModelNames}
-}
-
-type ModelInfos struct {
-	Models     map[string]Model
-	ModelNames []string
-}
-
-func browse(modelName string, page int) string {
-	response := get(endPoint + "/entries/read/" + modelName + "?page=" + fmt.Sprintf("%d", page))
-	return response.Body
-}
-
-func read(compoundID string) string {
-	response := get(endPoint + "/entries/read/" + compoundID)
-	return response.Body
-}
-
-func delete(compoundID string) string {
-	response := httpDelete(endPoint + "/entries/" + compoundID)
-	return response.Body
-}
-
-func save(compoundIDOrModel string, data JsonObject) string {
-	var etag string
-	var ok bool
-	if etag, ok = data["_etag"].(string); ok {
-	} else {
-		etag = ""
-	}
-	var response ResourceResult
-	if etag != "" {
-		response = postUpdate(endPoint+"/entries/"+compoundIDOrModel, toJson(data), etag)
-	} else {
-		response = post(endPoint+"/entries/"+compoundIDOrModel, toJson(data))
-	}
-
-	responseObject := parseJson(response.Body)
-	dataPart := responseObject["data"].(map[string]interface{})
-	entryId := dataPart["id"].(string)
-	if entryId == "" {
-		return ""
-	}
-	// check if compoundIDOrModel contains a slash somewhere
-	var cid string
-	if strings.Contains(compoundIDOrModel, "/") {
-		cid = compoundIDOrModel
-	} else {
-		cid = compoundIDOrModel + "/" + entryId
-	}
-
-	fmt.Println("Saving of " + cid + " returned " + response.ETag)
-	return cid
-}
-
 func main() {
 	if len(os.Args) > 1 {
-		// save, read, browse, delete
+		// save, read, browse, deleteEntry
 		if os.Args[1] == "save" {
-			compoundIDOrModel := os.Args[2]
-			jsonString := os.Args[3]
-			if jsonString == "-" {
-				streamCSV(compoundIDOrModel)
-				os.Exit(0)
-			}
-			data := parseJson(jsonString)
-			id := save(compoundIDOrModel, data)
-			if id == "" {
-				os.Exit(1)
-			}
-			fmt.Println(id)
-			os.Exit(0)
+			modelName := os.Args[2]
+			response := streamEntriesFromStdin(modelName)
+			fmt.Println(response)
+		} else if os.Args[1] == "save-models" {
+			fmt.Println(saveModelsFromStdin())
 		} else if os.Args[1] == "read" {
 			compoundID := os.Args[2]
-			response := read(compoundID)
-			pprint(response)
+			response := readEntry(compoundID)
+			fmt.Println(response)
 		} else if os.Args[1] == "browse" {
-			modelName := os.Args[2]
-			page := 1
-			if len(os.Args) > 3 {
-				page, _ = strconv.Atoi(os.Args[3])
-			}
-			response := browse(modelName, page)
-			pprint(response)
+			query := os.Args[2]
+			response := browse(query)
+			fmt.Println(response)
 		} else if os.Args[1] == "models" {
-			var listOfModels []Model
-			modelInfos := loadModelsFromAPI()
-			for _, modelName := range modelInfos.ModelNames {
-				listOfModels = append(listOfModels, modelInfos.Models[modelName])
+			modelName := ""
+			if len(os.Args) > 2 {
+				modelName = os.Args[2]
 			}
-			jsonStringOfModels := toJson(listOfModels)
-			pprint(jsonStringOfModels)
+			modelInfos := getModels(modelName)
+			fmt.Println(modelInfos)
+		} else if os.Args[1] == "relation" {
+			relationExpression := os.Args[2]
+			response := getRelation(relationExpression)
+			fmt.Println(response)
 		} else if os.Args[1] == "fields" {
 			response := getFieldTypes()
-			pprint(response)
+			fmt.Println(response)
 		} else if os.Args[1] == "delete" {
 			compoundID := os.Args[2]
 			if compoundID == "-" {
-				streamDelete()
+				deleteFromStdin()
 				os.Exit(0)
 			} else if strings.Contains(compoundID, "/") {
-				response := delete(compoundID)
-				pprint(response)
+				response := deleteEntry(compoundID)
+				fmt.Println(response)
 				os.Exit(0)
 			}
 			os.Exit(1)
@@ -142,38 +62,78 @@ func main() {
 	fmt.Println("  browse:  " + os.Args[0] + " browse <model> [page]")
 	fmt.Println("  read:    " + os.Args[0] + " read <compoundID>")
 	fmt.Println("  save:    " + os.Args[0] + " save <model> <json>")
-	fmt.Println("  delete:  " + os.Args[0] + " delete <compoundID>")
+	fmt.Println("  delete:  " + os.Args[0] + " deleteEntry <compoundID>")
 
 }
 
-func streamDelete() {
+func readEntry(compoundID string) string {
+	format := "tsv"
+	if strings.Contains(compoundID, ":") {
+		parts := strings.Split(compoundID, ":")
+		format = parts[0]
+		compoundID = parts[1]
+	}
+	response := get(endPoint + "/entries/read/" + compoundID + "?format=" + format)
+	return response.Body
+}
+
+func getRelation(expression string) string {
+	parts := strings.Split(expression, ".")
+	modelName := parts[0]
+	fieldName := parts[1]
+	response := get(endPoint + "/z/relation/" + modelName + "/" + fieldName)
+	return response.Body
+}
+
+func saveModelsFromStdin() string {
+	data, _ := io.ReadAll(os.Stdin)
+	response := post(endPoint+"/z/models", string(data))
+	return response.Body
+}
+
+func streamEntriesFromStdin(modelName string) string {
+	fmt.Println("Streaming data from stdin to " + modelName)
+	r := io.ReadCloser(os.Stdin)
+	defer r.Close()
+	result := postStream(endPoint+"/z/save/"+modelName, r)
+	return result.Body
+}
+
+func getFieldTypes() string {
+	response := queryRequest(apiKey, endPoint+"/models/fields")
+	return response.Body
+}
+
+func getModels(modelName string) string {
+	endpointURL := endPoint + "/z/models"
+	if modelName != "" {
+		endpointURL += "/" + modelName
+	}
+	apiResponse := queryRequest(apiKey, endpointURL)
+	return apiResponse.Body
+}
+
+func browse(query string) string {
+	parts := strings.Split(query, ":")
+	modelName := parts[0]
+	query = strings.Replace(query, modelName+":", "", 1)
+	queryArg := "q=" + url.QueryEscape(query)
+	formatArg := "format=tsv"
+	response := get(endPoint + "/entries/read/" + modelName + "?" + formatArg + "&" + queryArg)
+	return response.Body
+}
+
+func deleteEntry(compoundID string) string {
+	response := httpDelete(endPoint + "/entries/" + compoundID)
+	return response.Body
+}
+
+func deleteFromStdin() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		if strings.Contains(line, "/") {
-			pprint(delete(line))
+			fmt.Println(deleteEntry(line))
 		}
-	}
-}
-
-func streamCSV(compoundIDOrModel string) {
-	fmt.Println("Streaming CSV from stdin to " + compoundIDOrModel)
-	r := io.ReadCloser(os.Stdin)
-	defer r.Close()
-	result := postStream(endPoint+"/imports/"+compoundIDOrModel, r)
-	fmt.Println(result)
-}
-
-func pprint(jsonString string) {
-	fmt.Println(jsonPrettify(jsonString))
-}
-
-func createTestData() {
-	for i := 0; i < 10000; i++ {
-		newId := save("topics", JsonObject{
-			"name":   "Da new shite" + fmt.Sprintf("%d", i),
-			"status": "open",
-		})
-		fmt.Println(newId)
 	}
 }
